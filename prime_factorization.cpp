9124e5405cc83c9bce2282b4992e46e991989ce8
vo73444 <vo73444@mail.harpercollege.edu>
2024-02-04 20:08:21 -0600

    put the function into a header file

--- prime_factorization.cpp
+++ prime_factorization.cpp
@@ -1,35 +1,8 @@
 #include <iostream>
 
+#include "prime_factorization.hpp"
+
 using namespace std;
-
-void prime_factors(int n, int i);
-
-int main(){
-
-    int n = 0;
-
-    cout << "\nEnter a number greater than 1: ";
-
-    cin >> n;
-
-    while(n <= 1){
-        // keeps prompting user if they entered a number less than or equal to 1
-        cout << "\n\nI cannot calculate the prime factors of " << n << "\n\n";
-
-        cout << "\nEnter a number greater than 1: ";
-
-        cin >> n;
-    }
-
-    cout << "\n\nThe prime factors of " << n << " are ";
-
-    prime_factors(n, 2);
-
-    cout << "\n\n\n";
-
-
-    return 0;
-}
 
 void prime_factors(int n, int i){
 
